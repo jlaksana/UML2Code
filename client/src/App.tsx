1d9cd405d9def29ea659e275414d9f5588ed0ac4
Jonathan Laksana <jonathanalaksana@gmail.com>
2023-04-11 16:52:39 -0700

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import logo from "./assets/UML2.png";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src={logo} className="logo" alt="logo" />
+      </div>
+      <h1>My Senior Project!</h1>
+    </div>
+  );
+}
+
+export default App;
