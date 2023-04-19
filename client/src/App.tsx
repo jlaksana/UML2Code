e18feda7ceebf3cc4e8cc118b4b95234df5ab84d
Jonathan Laksana <jonathanalaksana@gmail.com>
2023-04-18 17:14:31 -0700

    add mui theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
+import { ThemeProvider } from '@mui/material/styles';
 import { Route, Routes } from 'react-router-dom';
 import Editor from './components/Editor';
 import NotFound from './components/NotFound';
 import StartMenu from './components/StartMenu';
+import theme from './theme';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<StartMenu />} />
-      <Route path="/:id" element={<Editor />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<StartMenu />} />
+        <Route path="/:id" element={<Editor />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5364FE',
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+});
+
+export default theme;
