cfb8bb543f1daecead656e40492d75bc62b9aea5
Jonathan Laksana <jonathanalaksana@gmail.com>
2023-04-25 15:14:05 -0700

    update diagram schema

--- server/src/models/diagram.ts
+++ server/src/models/diagram.ts
@@ -2,21 +2,22 @@ import { Document, Schema, model } from 'mongoose';
 
 interface IDiagram extends Document {
   _id: number;
-  created: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const diagramSchema = new Schema<IDiagram>({
-  _id: {
-    type: Number,
-    min: 1000,
-    max: 9999,
+const diagramSchema = new Schema<IDiagram>(
+  {
+    _id: {
+      type: Number,
+      min: 1000,
+      max: 9999,
+    },
   },
-  created: {
-    type: Date,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Diagram = model<IDiagram>('Diagram', diagramSchema);
 
 export default Diagram;
+export { IDiagram };
--- server/src/tests/diagram.test.ts
+++ server/src/tests/diagram.test.ts
@@ -30,25 +30,23 @@ describe('Diagram Schema', () => {
   it('should be able to save a valid diagram', async () => {
     const diagram1 = new Diagram({
       _id: 1234,
-      created: new Date(),
     });
     const savedDiagram1 = await diagram1.save();
     expect(savedDiagram1._id).toEqual(diagram1._id);
-    expect(savedDiagram1.created.getTime()).toEqual(diagram1.created.getTime());
+    expect(savedDiagram1.createdAt).not.toBeUndefined();
+    expect(savedDiagram1.updatedAt).not.toBeUndefined();
 
     const diagram2 = new Diagram({
       _id: '1235',
-      created: new Date(),
     });
     const savedDiagram2 = await diagram2.save();
     expect(savedDiagram2._id).toEqual(1235);
-    expect(savedDiagram2.created.getTime()).toEqual(diagram2.created.getTime());
+    expect(savedDiagram2.createdAt).not.toBeUndefined();
   });
 
   it('should not save a diagram with an invalid id', async () => {
     const diagram = new Diagram({
       _id: 999,
-      created: new Date(),
     });
     await expect(diagram.save()).rejects.toThrow();
   });
@@ -56,27 +54,16 @@ describe('Diagram Schema', () => {
   it('should not save a diagram with a duplicate id', async () => {
     const diagram1 = new Diagram({
       _id: 1234,
-      created: new Date(),
     });
     await diagram1.save();
     const diagram2 = new Diagram({
       _id: 1234,
-      created: new Date(),
     });
     await expect(diagram2.save()).rejects.toThrow();
   });
 
   it('should not save a diagram without an id', async () => {
-    const diagram = new Diagram({
-      created: new Date(),
-    });
-    await expect(diagram.save()).rejects.toThrow();
-  });
-
-  it('should not save a diagram without a created date', async () => {
-    const diagram = new Diagram({
-      _id: 1234,
-    });
+    const diagram = new Diagram({});
     await expect(diagram.save()).rejects.toThrow();
   });
 });
