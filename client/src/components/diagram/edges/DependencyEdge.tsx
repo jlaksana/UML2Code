d2274ab76b664302028695b39b5aa26e39a322c2
jonathanalaksana <jonathanalaksana@gmail.com>
2023-08-26 18:10:00 -0700

    fix dependency edge marker

--- client/src/components/diagram/edges/DependencyEdge.tsx
+++ client/src/components/diagram/edges/DependencyEdge.tsx
@@ -1,6 +1,8 @@
 import { BaseEdge, EdgeProps, getBezierPath } from 'reactflow';
+import { getMarkerRotation } from './edgeUtils';
 
 function DependencyEdge({
+  id,
   sourceX,
   sourceY,
   targetX,
@@ -17,11 +19,33 @@ function DependencyEdge({
     targetPosition,
   });
   return (
-    <BaseEdge
-      path={edgePath}
-      style={{ strokeDasharray: '5, 5' }}
-      markerStart="url(#1__height=50&type=arrow&width=50)"
-    />
+    <>
+      <svg style={{ position: 'absolute', top: 1000, left: 1000 }}>
+        <defs>
+          <marker
+            id={id}
+            viewBox="0 0 40 40"
+            markerHeight={70}
+            markerWidth={70}
+            refX={12}
+            refY={8}
+            orient={getMarkerRotation(sourcePosition)}
+            fill="none"
+            stroke="#555"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M18 15l-6-6-6 6" />
+          </marker>
+        </defs>
+      </svg>
+      <BaseEdge
+        path={edgePath}
+        style={{ strokeDasharray: '5, 5' }}
+        markerStart={`url(#${id})`}
+      />
+    </>
   );
 }
 
