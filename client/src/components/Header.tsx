9e955969e99e23b90bf380fd5c769ba2e7506db5
Jonathan Laksana <jonathanalaksana@gmail.com>
2023-09-21 21:08:37 -0700

    revise header to have actions menu instead

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,56 @@
-import CodeRoundedIcon from '@mui/icons-material/CodeRounded';
+import BugReportIcon from '@mui/icons-material/BugReport';
+// import CodeIcon from '@mui/icons-material/Code';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ShareIcon from '@mui/icons-material/Share';
-import { Button, Tooltip } from '@mui/material';
-import { Link, useParams } from 'react-router-dom';
+import {
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from '@mui/material';
+import { useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import logo from '../assets/UML2.png';
 import '../styles/Header.css';
 
 function Header() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
   const { diagramId } = useParams();
+
+  const navigate = useNavigate();
+
+  const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDocs = () => {
+    window.open(
+      'https://github.com/jlaksana/UML2Code/wiki/Documentation-Home',
+      '_blank'
+    );
+  };
+
+  const handleBug = () => {
+    window.open(
+      'https://github.com/jlaksana/UML2Code/issues/new?assignees=&labels=&projects=&template=bug_report.md&title=',
+      '_blank'
+    );
+  };
+
+  const navigateToStartMenu = () => {
+    handleClose();
+    navigate('/');
+  };
 
   return (
     <div className="header">
@@ -21,16 +65,52 @@ function Header() {
         <span>ID: {diagramId}</span>
       </div>
       <div className="right">
-        <Tooltip title="Share or print">
-          <Button variant="contained" startIcon={<ShareIcon />}>
-            Share
-          </Button>
-        </Tooltip>
-        <Tooltip title="Generate code">
-          <Button variant="contained" startIcon={<CodeRoundedIcon />}>
-            Code
-          </Button>
-        </Tooltip>
+        <IconButton
+          aria-label="more"
+          size="large"
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleOpen}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem>
+            <ListItemIcon>
+              <ShareIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>Share</ListItemText>
+          </MenuItem>
+          {/* <MenuItem>
+            <ListItemIcon>
+              <CodeIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>Code</ListItemText>
+          </MenuItem> */}
+          <MenuItem onClick={handleDocs}>
+            <ListItemIcon>
+              <MenuBookIcon fontSize="small" />
+            </ListItemIcon>
+            Documentation
+          </MenuItem>
+          <MenuItem onClick={handleBug}>
+            <ListItemIcon>
+              <BugReportIcon fontSize="small" />
+            </ListItemIcon>
+            Report Bug
+          </MenuItem>
+          <MenuItem onClick={navigateToStartMenu}>
+            <ListItemIcon>
+              <KeyboardReturnIcon fontSize="small" />
+            </ListItemIcon>
+            Start Menu
+          </MenuItem>
+        </Menu>
       </div>
     </div>
   );
