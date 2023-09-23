0943c2996639a82f482159a0437d0d7fc00424b6
Jonathan Laksana <jonathanalaksana@gmail.com>
2023-09-22 21:29:49 -0700

    fix cors

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -16,11 +16,20 @@ const interfaces_1 = __importDefault(require("./routes/interfaces"));
 const relationship_1 = __importDefault(require("./routes/relationship"));
 const app = (0, express_1.default)();
 // Middleware
-app.use(express_1.default.json());
-app.use((0, cors_1.default)({
-    origin: ['http://localhost:5173', 'https://uml2code.vercel.app'],
+const corsOptions = {
+    origin: ['http://localhost', 'https://uml2code.vercel.app'],
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'Access-Control-Allow-Methods',
+        'Access-Control-Request-Headers',
+    ],
     credentials: true,
-}));
+    enablePreflight: true,
+};
+app.use((0, cors_1.default)(corsOptions));
+app.options('*', (0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
 app.use((0, compression_1.default)());
 app.use((0, cookie_parser_1.default)());
 app.use(utilMiddleware_1.loggerMiddleware);
@@ -32,7 +41,7 @@ app.use('/api/enum', enums_1.default);
 app.use('/api/entity', entity_1.default);
 app.use('/api/relationship', relationship_1.default);
 app.get('/', (req, res) => {
-    res.send('Hello World from UML2Code API!');
+    res.send('Hello World from UML2Code Server!');
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
