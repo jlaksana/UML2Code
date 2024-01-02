72bf2c6f943965ec502bd15dd018d53d243aa1b8
jonathanalaksana <jonathanalaksana@gmail.com>
2024-01-01 23:15:07 -0800

    dev tag doc title

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,5 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 const setDocumentTitle = (title: string) => {
+  if (process.env.NODE_ENV === 'development') {
+    document.title = `(DEV) ${title} - UML2Code`;
+    return;
+  }
   document.title = `${title} - UML2Code`;
 };
 
